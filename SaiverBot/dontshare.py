1504cd56af4786939facb0368b7197420e56c2ea
davidbutler000123 <davidbutler000123@gmail.com>
2024-04-02 14:23:20 +0900

    SaiverBot is updated for calcMatrics api

--- SaiverBot/dontshare.py
+++ SaiverBot/dontshare.py
@@ -1,7 +1,10 @@
-PRIVATE_KEY = '' # your private key
+TOKEN_ADDR = 'A7vphAPCSfgGpZopMtD7AsV7nXYfCZHTLAJx8xQ583xG' # Token address
 
-BIRDEYE_KEY = '9ade21aa6dac44d...' # birdeye key
+PERIOD = 1 # 1, 3, 5, 15, 30 
 
 INTERVAL_TIME_TYPE = '1m' # 1m, 3m, 5m, 15m, 30m, 1H, 2H, 4H, 6H, 8H, 12H, 1D, 3D, 1W, 1M
 
+SERVER_URL = "http://95.217.146.177:5000/api/"
+
 EXPORT_EXCEL_PATH = "./output/out.xlsx"
+
